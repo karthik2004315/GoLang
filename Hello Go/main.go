806d8091003ef3f5f0402629de9e5740ce142efd
karthik2004315 <107571388+karthik2004315@users.noreply.github.com>
2023-06-15 19:45:19 +0530

    added two folders

---
+++ Hello Go/main.go
@@ -0,0 +1,11 @@
+package main;
+
+import "fmt";
+
+func main(){
+	fmt.Println("Hello Go!");
+}
+
+// To run a go program open the terminal and run this below comand
+
+// "go run main.go" this comman produces the output
