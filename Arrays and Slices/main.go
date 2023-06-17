a1a4e06338577d4ae8b812177a4f86740dcbf4da
karthik2004315 <107571388+karthik2004315@users.noreply.github.com>
2023-06-17 18:54:05 +0530

    added arrays and slices folder

---
+++ Arrays and Slices/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	fruits := [...]string{ "apple", "banana", "grapes"}
+
+	for _, value := range fruits{
+		fmt.Println(value)
+	}
+
+	slice1 = fruits[:1]
+	slice2 = fruits[1:]
+
+	fmt.Println(slice1)
+	fmt.Println(slice2)
+	slice3 := append(slice1, slice2...)
+	fmt.Println(slice3)
+
+	mappy := make(map[int]string)
+	mappy[1] = "first"
+	mappy[2] = "second"
+	mappy[3] = "third"
+	fmt.Println(mappy)
+}
