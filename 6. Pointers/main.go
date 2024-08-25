50b64ca8d67cd541d2cb9c7ab44aa90239a7fcbb
D. S.S.V Karthik <21131A0539@gvpce.ac.in>
2024-08-25 18:13:11 +0530

    corrected errors

--- 6. Pointers/main.go
+++ 6. Pointers/main.go
@@ -1,47 +1,47 @@
-// Do check the notes once
 package main
 
 import "fmt"
 
-func main(){
+// Function to update the value of an integer by reference
+func update(i *int) {
+	*i += 20
+}
+
+// Function to modify the first element of a slice
+func modify(str []int) {
+	str[0] = 10 // No need for pointers since slices are reference types
+}
+
+// Function to modify a map
+func modifyMaps(m map[string]int) {
+	m["third"] = 3 // No need for pointers since maps are reference types
+}
+
+func main() {
 	i := 10
 	var ptr1 *int = &i
-	fmt.Println("value is: ",*ptr1, "address is: ", ptr1)
+	fmt.Println("value is:", *ptr1, "address is:", ptr1)
 
 	var ptr2 = &i
-	fmt.Println("value is: ",*ptr2, "address is: ", ptr2)
+	fmt.Println("value is:", *ptr2, "address is:", ptr2)
 
 	ptr3 := &i
-	fmt.Println("value is: ",*ptr3, "address is: ", ptr3)\
+	fmt.Println("value is:", *ptr3, "address is:", ptr3)
 
-// Passing by reference
-
-	func update(i *int){
-		*i += 20
-	}
-
+	// Passing by reference
 	update(&i)
 	fmt.Println(i)
 
-// slices
-
-	func modify(str []int){
-		str[0] = 10           // see we dont need the concept of pointers
-	}
-
-	slice = []int{1, 2, 3}
+	// Slices
+	slice := []int{1, 2, 3}
 	modify(slice)
 	fmt.Println(slice)
 
-// maps 
-
-	func modifyMaps(m map[string]int){
-		m["third"] = 3        // see we didnt use the concept of pointers for maps
+	// Maps
+	mappy := map[string]int{
+		"first":  1,
+		"second": 2,
 	}
-
-	mappy := map[string]int
-	mappy["first"]  = 1
-	mappy["second"] = 2
 
 	modifyMaps(mappy)
 	fmt.Println(mappy)
